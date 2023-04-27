bb109fae0960b4fefd67a6c5e06f7611574984f3
NGAM TARYLL <taryllngam@gmail.com>
2023-04-27 10:26:18 +0100

    adding funtionality with js

--- script.js
+++ script.js
@@ -1,35 +1,79 @@
-let image = document.getElementById('image')
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn')
-let imgarr = ['./assets/images/1.jpg','./assets/images/2.jpg','./assets/images/3.jpg','./assets/images/4.jpg','./assets/images/5.jpg','./assets/images/6.jpg','./assets/images/7.jpg','./assets/images/8.jpg','./assets/images/9.jpg','./assets/images/10.jpg','./assets/images/11.jpg','./assets/images/12.jpg','./assets/images/13.jpg','./assets/images/14.jpg','./assets/images/15.jpg','./assets/images/16.jpg','./assets/images/17.jpg','./assets/images/18.jpg','./assets/images/19.jpg','./assets/images/20.jpg','./assets/images/21.jpg','./assets/images/22.jpg','./assets/images/23.jpg','./assets/images/24.jpg'];
+let image = document.getElementById("image");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+let imgarr = [
+  "./assets/images/1.jpg",
+  "./assets/images/2.jpg",
+  "./assets/images/3.jpg",
+  "./assets/images/4.jpg",
+  "./assets/images/5.jpg",
+  "./assets/images/6.jpg",
+  "./assets/images/7.jpg",
+  "./assets/images/8.jpg",
+  "./assets/images/9.jpg",
+  "./assets/images/10.jpg",
+  "./assets/images/11.jpg",
+  "./assets/images/12.jpg",
+  "./assets/images/13.jpg",
+  "./assets/images/14.jpg",
+  "./assets/images/15.jpg",
+  "./assets/images/16.jpg",
+  "./assets/images/17.jpg",
+  "./assets/images/18.jpg",
+  "./assets/images/19.jpg",
+  "./assets/images/20.jpg",
+];
+const display = document.getElementById('display')
+
+let nameArr = [
+   "SANTORINI",
+   "BORA BORA",
+   "KO SAMUI",
+   "HVAR",
+   "MAJORCA",
+   "KAUAI",
+   "BALI",
+   "MALDIVES",
+   "SEYCHELLES",
+   "FIJI",
+   "MALLORCA",
+   "SAMOA",
+   "ISLE OF SKYE",
+   "GALAPAGOS",
+   "HAWAII",
+   "MALDIVES",
+   "DALMATIAN ISLANDS",
+   "ARUBA",
+   "PALAWAN",
+   "OLGA"
+];
+
 
 let n = 0;
 
-
 const displayImages = () => {
-    image.src = imgarr[n]
-    n++
-    if(n === imgarr.length){
-        n = 0
-    }
-}
+  image.src = imgarr[n];
+  display.innerHTML = nameArr[n]
+  n++;
+  if (n === imgarr.length) {
+    n = 0;
+  }
+};
 
+setInterval(() => {
+  displayImages();
+}, 9000);
+console.log(image);
 
-setInterval(() =>{
-    displayImages();
-},9000)
-console.log(image)
-
-imgarr.length-1
+// imgarr.length - 1;
 const backwards = () => {
-    image.src = imgarr[n]
-    n--
-}
+  image.src = imgarr[n];
+  display.innerHtml = nameArr[n]
+  n--;
+  if (n > 0) {
+    n = imgarr.length - 1;
+  }
+};
 
-
-
-
-nextBtn.addEventListener('click', displayImages)
-prevBtn.addEventListener('click', backwards)
-
-
+nextBtn.addEventListener("click", displayImages);
+prevBtn.addEventListener("click", backwards);
